f166d9d9fd087696e458b81eaf9261f46634d43f
Yatish Jariyal <yatishjariyal@gmail.com>
2020-12-14 21:15:43 +0530

    added react router and created product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import { Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomePage />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/product/:id" component={ProductPage} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
