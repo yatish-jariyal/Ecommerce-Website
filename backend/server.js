cd3c2752de3df2f7a1f1ad1f60c90b105f1e59c1
Yatish Jariyal <yatishjariyal@gmail.com>
2020-12-15 19:06:33 +0530

    created backend, fetching data from backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send('api is running')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product)
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductPage = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data)
+    };
+    fetchProduct();
+  }, [match]);
   return (
     <>
       <Link to="/" className="btn btn-light my-3">
@@ -52,7 +60,11 @@ const ProductPage = ({ match }) => {
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className="btn btn-block" type="button" disabled={product.countInStock === 0}>
+                <Button
+                  className="btn btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
                   Add To Cart
                 </Button>
               </ListGroup.Item>
